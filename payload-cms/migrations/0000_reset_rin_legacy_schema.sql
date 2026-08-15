1e1d14b9286f49ccb5f4a1960e01ec652fdc8f10
fullcolour <79559280+fullcolour@users.noreply.github.com>
2026-08-16 01:10:16 +0900

    Make rin reset migration D1-compatible

--- payload-cms/migrations/0000_reset_rin_legacy_schema.sql
+++ payload-cms/migrations/0000_reset_rin_legacy_schema.sql
@@ -1,5 +1,3 @@
-PRAGMA foreign_keys=OFF;
-
 DROP TABLE IF EXISTS `comments`;
 DROP TABLE IF EXISTS `visits`;
 DROP TABLE IF EXISTS `feed_hashtags`;
@@ -8,7 +6,4 @@ DROP TABLE IF EXISTS `friends`;
 DROP TABLE IF EXISTS `hashtags`;
 DROP TABLE IF EXISTS `info`;
 DROP TABLE IF EXISTS `moments`;
-DROP TABLE IF EXISTS `_cf_KV`;
 DROP TABLE IF EXISTS `users`;
-
-PRAGMA foreign_keys=ON;
