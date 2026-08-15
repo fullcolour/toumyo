924a655379f2686598ae940b8245a43d7410829c
fullcolour <79559280+fullcolour@users.noreply.github.com>
2026-08-16 00:43:12 +0900

    Track Payload migration metadata

---
+++ payload-cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260815_154117 from './20260815_154117';
+
+export const migrations = [
+  {
+    up: migration_20260815_154117.up,
+    down: migration_20260815_154117.down,
+    name: '20260815_154117'
+  },
+];
